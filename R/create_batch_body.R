42172f753ec791b527bde1c07c26062a7692ab73
Yaoxiang Li <liyaoxiang@outlook.com>
2024-10-03 15:32:53 -0400

    Update create_batch_body.R

--- R/create_batch_body.R
+++ R/create_batch_body.R
@@ -15,54 +15,52 @@
 #' @return If all inputs are all correctly formatted, a string of a POST request will be returned for the result.
 #' @examples
 #'
-#' batch_body <- create_batch_body('all-except-peptides',
-#'                                 '["all-except-mine"]',
-#'                                 'mz',
-#'                                 'positive',
-#'                                 '["M+H","M+Na"]',
-#'                                 10,
-#'                                 'ppm',
-#'                                 c(670.4623, 1125.2555, 602.6180))
+#' batch_body <- create_batch_body(
+#'   "all-except-peptides",
+#'   '["all-except-mine"]',
+#'   "mz",
+#'   "positive",
+#'   '["M+H","M+Na"]',
+#'   10,
+#'   "ppm",
+#'   c(670.4623, 1125.2555, 602.6180)
+#' )
 #'
-#' batch_body <- create_batch_body('all-except-peptides',
-#'                                 '["all-except-mine"]',
-#'                                 'mz',
-#'                                 'negative',
-#'                                 '["M-H","M+Cl"]',
-#'                                 10,
-#'                                 'ppm',
-#'                                 c(670.4623, 1125.2555, 602.6180))
+#' batch_body <- create_batch_body(
+#'   "all-except-peptides",
+#'   '["all-except-mine"]',
+#'   "mz",
+#'   "negative",
+#'   '["M-H","M+Cl"]',
+#'   10,
+#'   "ppm",
+#'   c(670.4623, 1125.2555, 602.6180)
+#' )
 #'
 #' \dontrun{
 #' create_batch_body(c(670.4623, 1125.2555, 602.6180))
 #' }
-#'
-#' @author Yaoxiang Li \email{yl814@georgetown.edu}
-#'
-#' Georgetown University, USA
-#'
-#' License: GPL (>= 3)
 #' @export
 #'
-create_batch_body <- function(metabolites_type = 'all-except-peptides',
-                              databases        = '["all-except-mine"]',
-                              masses_mode      = 'mz',
-                              ion_mode         = 'positive',
-                              adducts          = '["M+H","M+Na"]',
-                              tolerance        = 10,
-                              tolerance_mode   = 'ppm',
+create_batch_body <- function(metabolites_type = "all-except-peptides",
+                              databases = '["all-except-mine"]',
+                              masses_mode = "mz",
+                              ion_mode = "positive",
+                              adducts = '["M+H","M+Na"]',
+                              tolerance = 10,
+                              tolerance_mode = "ppm",
                               unique_mz) {
-
-  masses    <- paste(unique_mz, collapse = ",")
+  masses <- paste(unique_mz, collapse = ",")
   tolerance <- as.character(tolerance)
   post_body <- paste0(
-    '{"metabolites_type":"' , metabolites_type,
-    '","databases":'        , databases,
-    ',"masses_mode":"'      , masses_mode,
-    '","ion_mode":"'        , ion_mode,
-    '","adducts":'          , adducts,
-    ',"tolerance":'         , tolerance,
-    ',"tolerance_mode":"'   , tolerance_mode,
-    '","masses":['          , masses,
-    ']}')
+    '{"metabolites_type":"', metabolites_type,
+    '","databases":', databases,
+    ',"masses_mode":"', masses_mode,
+    '","ion_mode":"', ion_mode,
+    '","adducts":', adducts,
+    ',"tolerance":', tolerance,
+    ',"tolerance_mode":"', tolerance_mode,
+    '","masses":[', masses,
+    "]}"
+  )
 }
