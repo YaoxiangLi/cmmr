ba6007fdbe9920c70be0b41bca4e788da119b0bc
Yaoxiang Li <liyaoxiang@outlook.com>
2024-10-03 15:32:51 -0400

    Update create_advanced_batch_body.R

--- R/create_advanced_batch_body.R
+++ R/create_advanced_batch_body.R
@@ -20,48 +20,43 @@
 #'
 #' @return If all inputs are all correctly formatted, a dataframe will be returned for the result.
 #'
-#' @author Yaoxiang Li \email{yl814@georgetown.edu}
-#'
-#' Georgetown University, USA
-#'
-#' License: GPL (>= 3)
 #' @export
 #'
 create_advanced_batch_body <- function(
-  chemical_alphabet   = 'all',
-  modifiers_type      = 'none',
-  metabolites_type    = 'all-except-peptides',
-  databases           = '["hmdb"]',
-  masses_mode         = 'mz',
-  ion_mode            = 'positive',
-  adducts             = '["all"]',
-  deuterium           = 'false',
-  tolerance           = '7.5',
-  tolerance_mode      = 'ppm',
-  masses              = '[400.3432, 288.2174]',
-  all_masses          = '[]',
-  retention_times     = '[18.842525, 4.021555]',
-  all_retention_times = '[]',
-  composite_spectra   = paste0(
-    '[[{ "mz": 400.3432, "intensity": 307034.88 }, ',
-    '{ "mz": 311.20145, "intensity": 400.03336 }]]'
-  )) {
-
-  post_body <- paste0('{"chemical_alphabet": "' , chemical_alphabet,
-                      '","modifiers_type": "'   , modifiers_type,
-                      '","metabolites_type": "' , metabolites_type,
-                      '","databases": '         , databases,
-                      ',"masses_mode": "'       , masses_mode,
-                      '","ion_mode": "'         , ion_mode,
-                      '","adducts": '           , adducts,
-                      ',"deuterium": '          , deuterium,
-                      ',"tolerance": '          , as.character(tolerance),
-                      ',"tolerance_mode":"'     , tolerance_mode,
-                      '","masses": '            , masses,
-                      ',"all_masses": '         , all_masses,
-                      ',"retention_times": '    , retention_times,
-                      ',"all_retention_times": ', all_retention_times,
-                      ',"composite_spectra": '  , composite_spectra,
-                      '}')
-
+    chemical_alphabet = "all",
+    modifiers_type = "none",
+    metabolites_type = "all-except-peptides",
+    databases = '["hmdb"]',
+    masses_mode = "mz",
+    ion_mode = "positive",
+    adducts = '["all"]',
+    deuterium = "false",
+    tolerance = "7.5",
+    tolerance_mode = "ppm",
+    masses = "[400.3432, 288.2174]",
+    all_masses = "[]",
+    retention_times = "[18.842525, 4.021555]",
+    all_retention_times = "[]",
+    composite_spectra = paste0(
+      '[[{ "mz": 400.3432, "intensity": 307034.88 }, ',
+      '{ "mz": 311.20145, "intensity": 400.03336 }]]'
+    )) {
+  post_body <- paste0(
+    '{"chemical_alphabet": "', chemical_alphabet,
+    '","modifiers_type": "', modifiers_type,
+    '","metabolites_type": "', metabolites_type,
+    '","databases": ', databases,
+    ',"masses_mode": "', masses_mode,
+    '","ion_mode": "', ion_mode,
+    '","adducts": ', adducts,
+    ',"deuterium": ', deuterium,
+    ',"tolerance": ', as.character(tolerance),
+    ',"tolerance_mode":"', tolerance_mode,
+    '","masses": ', masses,
+    ',"all_masses": ', all_masses,
+    ',"retention_times": ', retention_times,
+    ',"all_retention_times": ', all_retention_times,
+    ',"composite_spectra": ', composite_spectra,
+    "}"
+  )
 }
