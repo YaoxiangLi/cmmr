8ebbae00e37898a59e7c1aeab72aac8973df1b0a
Yaoxiang Li <liyaoxiang@outlook.com>
2024-10-04 14:23:04 -0400

    Adds support for "InChIKey" and "Pathways"

--- R/batch-search.R
+++ R/batch-search.R
@@ -37,19 +37,22 @@ batch_search <- function(cmm_url = "https://ceumass.eps.uspceu.es/api/v3/batch",
                          tolerance = 10,
                          tolerance_mode = "ppm",
                          unique_mz) {
+
+  # Updated columns to save without SMILES
   columns_to_save <- c(
     "identifier", "EM", "name", "formula", "adduct",
     "molecular_weight", "error_ppm", "ionizationScore", "finalScore",
     "kegg_compound", "kegg_uri", "hmdb_compound", "hmdb_uri",
     "lipidmaps_compound", "lipidmaps_uri", "metlin_compound", "metlin_uri",
-    "pubchem_compound", "pubchem_uri"
+    "pubchem_compound", "pubchem_uri", "inChiKey", "pathways"
   )
 
+  # Updated column names without SMILES
   columns_to_name <- c(
     "identifier", "experimental_mass", "name", "formula", "adduct",
     "molecular_weight", "error_ppm", "ionization_score", "final_score",
     "Kegg", "Kegg_URI", "HMDB", "HMDB_URI", "LipidMaps", "LipidMaps_URI",
-    "Metlin", "Metlin_URI", "PubChem", "PubChem_URI"
+    "Metlin", "Metlin_URI", "PubChem", "PubChem_URI", "InChIKey", "Pathways"
   )
 
   body <- create_batch_body(metabolites_type, databases, masses_mode, ion_mode, adducts, tolerance, tolerance_mode, unique_mz)
@@ -69,28 +72,32 @@ batch_search <- function(cmm_url = "https://ceumass.eps.uspceu.es/api/v3/batch",
 
     pb <- progress::progress_bar$new(
       format = "  Parsing database search results [:bar] :percent in :elapsed",
-      total = length(json_file) - 1,
+      total = length(json_file),
       clear = FALSE,
       width = 100
     )
 
-    df <- json_file[[1]]
-    df <- as.data.frame(df[names(df) %in% columns_to_save])
-    if (length(json_file) > 1) {
-      for (i in 2:length(json_file)) {
-        pb$tick()
-        dfi <- json_file[[i]]
-        dfi <- as.data.frame(dfi[names(dfi) %in% columns_to_save])
-        df <- rbind(df, dfi)
-      }
+    # Initialize an empty list to store each result's data
+    results_list <- list()
+
+    for (i in seq_along(json_file)) {
+      pb$tick()
+      record <- json_file[[i]]
+      # Ensure all columns are present; if not, add NA for missing fields
+      result <- sapply(columns_to_save, function(col) ifelse(!is.null(record[[col]]), record[[col]], NA))
+      results_list[[i]] <- result
     }
 
-    df <- df[, columns_to_save]
+    # Convert the list of results to a dataframe
+    df <- do.call(rbind, results_list)
+    df <- as.data.frame(df, stringsAsFactors = FALSE)
     colnames(df) <- columns_to_name
 
     cli::cli_alert_success("Database search results parsed successfully.")
     return(df)
+
   } else {
     cli::cli_alert_danger("Failed to connect to the API service (Status: {r$status_code}).")
+    return(NULL)
   }
 }
