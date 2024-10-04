094e848b68a136342075dd6e9390510dc669c5c7
Yaoxiang Li <liyaoxiang@outlook.com>
2024-10-04 13:22:13 -0400

    Update batch-search-advanced.R

--- R/batch-search-advanced.R
+++ R/batch-search-advanced.R
@@ -1,76 +1,72 @@
-#' Encapsulation of CEU Mass Mediator advanced batch search API.
+#' Advanced Batch Search using CEU Mass Mediator API
 #'
-#' \code{advanced_batch_search} returns the body string of a POST request.
-#'   using the following code to install the dependencies:
-#'   install.packages(c("httr", "progress", "RJSONIO"))
+#' \code{advanced_batch_search} performs an advanced batch search on the CEU Mass Mediator API
+#' and returns a dataframe of search results.
 #'
-#' @param cmm_url 'http://ceumass.eps.uspceu.es/mediator/api/v3/advancedbatch' or your local one
-#' @param chemical_alphabet "CHNOPS", "CHNOPSCL", "ALL"
-#' @param modifiers_type "none", "NH3", "HCOO", "CH3COO", "HCOONH3", "CH3COONH3"
-#' @param metabolites_type "all-except-peptides", "only-lipids", "all-including-peptides"
-#' @param databases "all", "all-except-mine", "HMDB", "LipidMaps", "Metlin", "Kegg", "in-house", "mine"
-#' @param masses_mode "neutral", "mz"
-#' @param ion_mode "positive", "negative"
-#' @param adducts for positive mode ["M+H", "M+2H", "M+Na", "M+K", "M+NH4", "M+H-H2O"] for negative mode ["M-H", "M+Cl", "M+FA-H", "M-H-H2O"], for neutral ["M"]
-#' @param deuterium boolean 'true' 'false'
-#' @param tolerance double (Range: [0..100])
-#' @param tolerance_mode "ppm", "mDa"
-#' @param masses double
-#' @param all_masses array of doubles
-#' @param retention_times double
-#' @param all_retention_times array of doubles
-#' @param composite_spectra array of arrays of spectra_object
+#' @param cmm_url A character string specifying the CEU Mass Mediator API endpoint.
+#'   Default is \code{"https://ceumass.eps.uspceu.es/api/v3/advancedbatch"}.
+#' @param chemical_alphabet A character string specifying the chemical alphabet to use.
+#'   Options are \code{"CHNOPS"}, \code{"CHNOPSCL"}, or \code{"ALL"}.
+#' @param modifiers_type A character string specifying the modifier type.
+#'   Options are \code{"none"}, \code{"NH3"}, \code{"HCOO"}, \code{"CH3COO"}, \code{"HCOONH3"}, or \code{"CH3COONH3"}.
+#' @param metabolites_type A character string specifying the metabolites type.
+#'   Options are \code{"all-except-peptides"}, \code{"only-lipids"}, or \code{"all-including-peptides"}.
+#' @param databases A JSON-formatted character string specifying the databases to search.
+#'   Examples include \code{'["all"]'}, \code{'["HMDB"]'}, \code{'["LipidMaps"]'}.
+#' @param masses_mode A character string specifying the masses mode. Options are \code{"neutral"} or \code{"mz"}.
+#' @param ion_mode A character string specifying the ionization mode. Options are \code{"positive"}, \code{"negative"}, or \code{"neutral"}.
+#' @param adducts A JSON-formatted character string specifying the adducts to include in the search.
+#'   Examples include \code{'["M+H","M+Na"]'} for positive mode.
+#' @param deuterium A logical value indicating whether to consider deuterium substitutions. \code{TRUE} or \code{FALSE}.
+#' @param tolerance A numeric value specifying the mass tolerance (Range: \code{0} to \code{100}).
+#' @param tolerance_mode A character string specifying the tolerance mode. Options are \code{"ppm"} or \code{"mDa"}.
+#' @param masses A numeric vector of masses to search.
+#' @param all_masses A JSON-formatted character string representing an array of mass arrays.
+#' @param retention_times A numeric vector of retention times corresponding to the masses.
+#' @param all_retention_times A JSON-formatted character string representing an array of retention time arrays.
+#' @param composite_spectra A JSON-formatted character string representing composite spectra.
 #'
-#' @return If all inputs are all correctly formatted, a dataframe will be returned for the result.
-#' @examples df <- advanced_batch_search(
-#'   cmm_url = paste0(
-#'     "http://ceumass.eps.uspceu.es/mediator/api/v3/",
-#'     "advancedbatch"
-#'   ),
-#'   chemical_alphabet = "all",
+#' @return A dataframe containing the search results from the CEU Mass Mediator API.
+#' @examples
+#' \dontrun{
+#' df <- advanced_batch_search(
+#'   cmm_url = "https://ceumass.eps.uspceu.es/api/v3/advancedbatch",
+#'   chemical_alphabet = "ALL",
 #'   modifiers_type = "none",
 #'   metabolites_type = "all-except-peptides",
-#'   databases = '["hmdb"]',
+#'   databases = '["HMDB"]',
 #'   masses_mode = "mz",
 #'   ion_mode = "positive",
 #'   adducts = '["all"]',
-#'   deuterium = "false",
-#'   tolerance = "7.5",
+#'   deuterium = FALSE,
+#'   tolerance = 7.5,
 #'   tolerance_mode = "ppm",
-#'   masses = "[400.3432, 288.2174]",
+#'   masses = c(400.3432, 288.2174),
 #'   all_masses = "[]",
-#'   retention_times = "[18.842525, 4.021555]",
+#'   retention_times = c(18.842525, 4.021555),
 #'   all_retention_times = "[]",
-#'   composite_spectra = paste0(
-#'     '[[{ "mz": 400.3432, "intensity": 307034.88 }, ',
-#'     '{ "mz": 311.20145, "intensity": 400.03336 }]]'
-#'   )
+#'   composite_spectra = '[ [ { "mz": 400.3432, "intensity": 307034.88 }, { "mz": 311.20145, "intensity": 400.03336 } ] ]'
 #' )
+#' }
 #' @export
-#'
 advanced_batch_search <- function(
-    cmm_url = paste0(
-      "https://ceumass.eps.uspceu.es/api/v3/advancedbatch",
-      "advancedbatch"
-    ),
-    chemical_alphabet = "all",
+    cmm_url = "https://ceumass.eps.uspceu.es/api/v3/advancedbatch",
+    chemical_alphabet = "ALL",
     modifiers_type = "none",
     metabolites_type = "all-except-peptides",
-    databases = '["hmdb"]',
+    databases = '["HMDB"]',
     masses_mode = "mz",
     ion_mode = "positive",
     adducts = '["all"]',
-    deuterium = "false",
-    tolerance = "7.5",
+    deuterium = FALSE,
+    tolerance = 7.5,
     tolerance_mode = "ppm",
-    masses = "[400.3432, 288.2174]",
+    masses = c(400.3432, 288.2174),
     all_masses = "[]",
-    retention_times = "[18.842525, 4.021555]",
+    retention_times = c(18.842525, 4.021555),
     all_retention_times = "[]",
-    composite_spectra = paste0(
-      '[[{ "mz": 400.3432, "intensity": 307034.88 }, ',
-      '{ "mz": 311.20145, "intensity": 400.03336 }]]'
-    )) {
+    composite_spectra = '[ [ { "mz": 400.3432, "intensity": 307034.88 }, { "mz": 311.20145, "intensity": 400.03336 } ] ]'
+) {
   post_body <- create_advanced_batch_body(
     chemical_alphabet, modifiers_type,
     metabolites_type, databases, masses_mode, ion_mode, adducts, deuterium,
@@ -78,72 +74,58 @@ advanced_batch_search <- function(
     all_retention_times, composite_spectra
   )
 
-  if (cmm_url == "https://ceumass.eps.uspceu.es/api/v3/advancedbatch") {
-    cat("Using the CEU Mass Mediator server API.\n")
-  } else {
-    cat("Using the local/3rd party server API:\n")
-    cat(paste0(cmm_url, "\n"))
-  }
+  cli::cli_alert_info("Connecting to the CEU Mass Mediator API at {cmm_url}...")
 
   r <- httr::POST(url = cmm_url, body = post_body, httr::content_type("application/json"))
 
-
   if (r$status_code == 200) {
-    columns_to_save <- c(
-      "RT", "adductRelationScore", "RTscore",
-      "identifier", "EM", "name", "formula", "adduct",
-      "molecular_weight", "error_ppm",
-      "ionizationScore", "finalScore",
-      "kegg_compound", "kegg_uri",
-      "hmdb_compound", "hmdb_uri",
-      "lipidmaps_compound", "lipidmaps_uri",
-      "metlin_compound", "metlin_uri",
-      "pubchem_compound", "pubchem_uri"
-    )
+    cli::cli_alert_success("API request successful (Status: {r$status_code}).")
 
-    columns_to_name <- c(
-      "RT", "adductRelationScore", "RTscore",
-      "Identifier", "Experimental.mass", "Name", "Formula", "Adduct",
-      "Molecular.Weight", "PPM.Error",
-      "Ionization.Score", "Final.Score",
-      "Kegg", "Kegg_URI",
-      "HMDB", "HMDB_URI",
-      "LipidMaps", "LipidMaps_URI",
-      "Metlin", "Metlin_URI",
-      "PubChem", "PubChem_URI"
-    )
-
-    cat(paste0("Status: ", r$status_code, ", Success!\n"))
-    cat(paste0("Date: ", r$date, "\n"))
     json_file <- RJSONIO::fromJSON(httr::content(r, "text", encoding = "UTF-8"))$results
     if (length(json_file) == 0) {
-      cat("No compounds found in the database search.\n")
-      return("No compounds found in the database search.")
+      cli::cli_alert_warning("No compounds found in the database search.")
+      return(NULL)
     }
 
     pb <- progress::progress_bar$new(
       format = "  Parsing database search results [:bar] :percent in :elapsed",
-      total  = length(json_file) - 1,
+      total  = length(json_file),
       clear  = FALSE,
       width  = 100
     )
 
-    df <- json_file[[1]]
-    df <- as.data.frame(df[names(df) %in% columns_to_save])
-    if (length(json_file) > 1) {
-      for (i in 2:length(json_file)) {
-        pb$tick()
-        dfi <- json_file[[i]]
-        dfi <- as.data.frame(dfi[names(dfi) %in% columns_to_save])
-        df <- rbind(df, dfi)
-      }
-    }
+    columns_to_save <- c(
+      "RT", "adductRelationScore", "RTscore",
+      "identifier", "EM", "name", "formula", "adduct",
+      "molecular_weight", "error_ppm", "ionizationScore", "finalScore",
+      "kegg_compound", "kegg_uri", "hmdb_compound", "hmdb_uri",
+      "lipidmaps_compound", "lipidmaps_uri", "metlin_compound", "metlin_uri",
+      "pubchem_compound", "pubchem_uri"
+    )
 
+    columns_to_name <- c(
+      "retention_time", "adduct_relation_score", "RT_score",
+      "identifier", "experimental_mass", "name", "formula", "adduct",
+      "molecular_weight", "error_ppm", "ionization_score", "final_score",
+      "KEGG", "KEGG_URI", "HMDB", "HMDB_URI", "LipidMaps", "LipidMaps_URI",
+      "Metlin", "Metlin_URI", "PubChem", "PubChem_URI"
+    )
+
+    df_list <- lapply(seq_along(json_file), function(i) {
+      pb$tick()
+      record <- json_file[[i]]
+      as.data.frame(record[names(record) %in% columns_to_save], stringsAsFactors = FALSE)
+    })
+
+    df <- do.call(rbind, df_list)
     df <- df[, columns_to_save]
     colnames(df) <- columns_to_name
+
+    cli::cli_alert_success("Database search results parsed successfully.")
     return(df)
+
   } else {
-    cat(paste0("Status: ", r$status_code, ", Fail to connect the API service!\n"))
-    cat(paste0("Date: ", r$date, "\n"))
+    cli::cli_alert_danger("Failed to connect to the API service (Status: {r$status_code}).")
+    return(NULL)
   }
 }
